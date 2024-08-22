eafb13490cb3baa7392f7b1ff382bfc86b46405b
EmyAndrade <168127434+EmyAndrade@users.noreply.github.com>
2024-08-22 14:13:59 -0300

    rodape e navbar

--- frontend/src/Components/Navegacao.jsx
+++ frontend/src/Components/Navegacao.jsx
@@ -1,45 +1,48 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import "../css/Navbar.css";
+import Logo from "../img/logo.png";
 
 function ColorSchemesExample() {
+  const location = useLocation(); // Obtém a localização atual
+
+  // Função para verificar se o link está ativo
+  const isActive = (path) => location.pathname === path;
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar expand="lg" className="custom-navbar" >
       <Container>
-        <Navbar.Brand as={Link} to="/">Brand</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/"><img src={Logo} alt="" /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link as={Link} to="/cadastroempresa">
+            <Nav.Link as={Link} to="/cadastroempresa" className={isActive("/cadastroempresa") ? "active" : ""}>
               Para sua empresa
             </Nav.Link>
-            <Nav.Link as={Link} to="/contato">
+            <Nav.Link as={Link} to="/contato" className={isActive("/contato") ? "active" : ""}>
               Contato
             </Nav.Link>
-            <Nav.Link as={Link} to="/cadastro">
-              Cadastro
-            </Nav.Link>
-            <Nav.Link as={Link} to="/cadastroPsicologos">
+            <Nav.Link as={Link} to="/cadastroPsicologos"  className={isActive("/cadastroPsicologos") ? "active" : ""}>
               Cadastro de psicólogos
             </Nav.Link>
-            <Nav.Link as={Link} to="/listaUsuarios">
+            <Nav.Link as={Link} to="/listaUsuarios" className={isActive("/listaUsuarios") ? "active" : ""}>
               Lista de Usuários
             </Nav.Link>
-            <Nav.Link as={Link} to="/agendarconsulta">
+            <Nav.Link as={Link} to="/agendarconsulta"  className={isActive("/agendarconsulta") ? "active" : ""}>
               Agendar Consultas
             </Nav.Link>
-            <Nav.Link as={Link} to="/planos">
+            <Nav.Link as={Link} to="/planos" className={isActive("/planos") ? "active" : ""}>
               Planos Empresáriais
             </Nav.Link>
-            <Nav.Link as={Link} to="/acessoFuncionarios">
+            <Nav.Link as={Link} to="/acessoFuncionarios" className={isActive("/acessoFuncionarios") ? "active" : ""}>
               Acesso Funcionarios
             </Nav.Link>
-            <Nav.Link as={Link} to="/perfil">
+            <Nav.Link as={Link} to="/perfil" className={isActive("/perfil") ? "active" : ""}>
               Perfil
             </Nav.Link>
-            <Nav.Link as={Link} to="/login">
+            <Nav.Link as={Link} to="/login" className={isActive("/login") ? "active" : ""}>
               Login
             </Nav.Link>
           </Nav>
