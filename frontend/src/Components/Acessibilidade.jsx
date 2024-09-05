47a345a7e80472a8a74ed96d3417ba0dfa3670ec
CarolSimplicio <princarol2007@gmail.com>
2024-09-05 08:40:46 -0300

    mudanças acessibilidade

--- frontend/src/Components/Acessibilidade.jsx
+++ frontend/src/Components/Acessibilidade.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
 import '../css/Acessibilidade.css';
-import { PersonStanding, X, AArrowDown, AArrowUp, Pointer, Contrast, Undo2 } from 'lucide-react';
+import { PersonStanding, X, AArrowDown, AArrowUp, Pointer, Contrast } from 'lucide-react';
 import Button from 'react-bootstrap/Button';
 import classNames from 'classnames';
 
-const AccessibilityPanel = () => {
-    const [fontSize, setFontSize] = useState(16);  // Tamanho da fonte inicial em pixels
+const Acessibilidade = () => {
+    const [fontSize, setFontSize] = useState(16);
     const [isPanelOpen, setIsPanelOpen] = useState(false);
     const [activeButtons, setActiveButtons] = useState({});
 
@@ -30,15 +30,15 @@ const AccessibilityPanel = () => {
     return (
         <>
             <button className="accessibility-toggle" onClick={handleTogglePanel} aria-label="Abrir painel de acessibilidade">
-                <PersonStanding />
+                <PersonStanding/>
             </button>
 
             {isPanelOpen && (
-                <div className="accessibility-panel">
+                <div className="accessibilidade-painel">
                     <button className="accessibility-close" onClick={handleTogglePanel} aria-label="Fechar painel de acessibilidade"><X /></button>
                     <h3 className='accessibility-title'>Acessibilidade</h3>
                     <div className="accessibility-content">
-                        {/* Ajuste de Tamanho da Fonte */}
+                        {/* tamanho da Fonte */}
                         <div className="accessibility-section font-adjustment">
                             <h5>Ajustar Tamanho da Fonte</h5>
                             <div className="font-size-controls">
@@ -49,57 +49,57 @@ const AccessibilityPanel = () => {
                         </div>
                         {/* Outros Botões */}
                         <div className="accessibility-buttons">
-                            <Button 
-                                className={classNames('accessibility-button', { 'active': activeButtons['increaseCursor'] })} 
+                            <Button
+                                className={classNames('accessibility-button', { 'active': activeButtons['increaseCursor'] })}
                                 onClick={() => toggleClass('large-cursor', 'increaseCursor')}
                                 aria-label="Aumentar cursor"
                             >
                                 <Pointer /> Aumentar Cursor
                             </Button>
-                            <Button 
-                                className={classNames('accessibility-button', { 'active': activeButtons['darkMode'] })} 
+                            <Button
+                                className={classNames('accessibility-button', { 'active': activeButtons['darkMode'] })}
                                 onClick={() => toggleClass('dark-mode', 'darkMode')}
                                 aria-label="Modo escuro"
                             >
                                 <Contrast /> Modo Escuro
                             </Button>
-                            <Button 
-                                className={classNames('accessibility-button', { 'active': activeButtons['highContrast'] })} 
+                            <Button
+                                className={classNames('accessibility-button', { 'active': activeButtons['highContrast'] })}
                                 onClick={() => toggleClass('high-contrast', 'highContrast')}
                                 aria-label="Modo alto contraste"
                             >
                                 Modo Alto Contraste
                             </Button>
-                            <Button 
-                                className={classNames('accessibility-button', { 'active': activeButtons['letterSpacing'] })} 
-                                onClick={() => toggleClass('increase-letter-spacing', 'letterSpacing')}
+                            <Button
+                                className={classNames('accessibility-button', { 'active': activeButtons['letterSpacing'] })}
+                                onClick={() => toggleClass('espacamento', 'letterSpacing')}
                                 aria-label="Espaço entre letras"
                             >
                                 Espaço Entre Letras
                             </Button>
-                            <Button 
-                                className={classNames('accessibility-button', { 'active': activeButtons['lineHeight'] })} 
-                                onClick={() => toggleClass('increase-line-height', 'lineHeight')}
+                            <Button
+                                className={classNames('accessibility-button', { 'active': activeButtons['lineHeight'] })}
+                                onClick={() => toggleClass('espacoLinhas', 'lineHeight')}
                                 aria-label="Espaço entre linhas"
                             >
                                 Espaço Entre Linhas
                             </Button>
-                            <Button 
-                                className={classNames('accessibility-button', { 'active': activeButtons['increaseImages'] })} 
+                            <Button
+                                className={classNames('accessibility-button', { 'active': activeButtons['increaseImages'] })}
                                 onClick={() => toggleClass('increase-images', 'increaseImages')}
                                 aria-label="Aumentar imagens"
                             >
                                 Aumentar Imagens
                             </Button>
-                            <Button 
-                                className={classNames('accessibility-button', { 'active': activeButtons['changeFont'] })} 
+                            <Button
+                                className={classNames('accessibility-button', { 'active': activeButtons['changeFont'] })}
                                 onClick={() => toggleClass('font-arial', 'changeFont')}
                                 aria-label="Fonte Arial"
                             >
                                 Fonte Arial
                             </Button>
-                            <Button 
-                                className={classNames('accessibility-button', { 'active': activeButtons['textMagnifier'] })} 
+                            <Button
+                                className={classNames('accessibility-button', { 'active': activeButtons['textMagnifier'] })}
                                 onClick={() => toggleClass('text-magnifier', 'textMagnifier')}
                                 aria-label="Lupa de texto"
                             >
@@ -107,10 +107,10 @@ const AccessibilityPanel = () => {
                             </Button>
                         </div>
                     </div>
-                </div>
+                </div >
             )}
         </>
     );
 };
 
-export default AccessibilityPanel;
+export default Acessibilidade;
