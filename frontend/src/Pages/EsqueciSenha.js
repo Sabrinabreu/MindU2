aa16eb08fe197ab227e32b1ad411347a0dcd04c7
Sabrinabreu <sabrinabritodeabreu@gmail.com>
2024-10-08 10:03:23 -0300

    esqueci senha resposivo

--- frontend/src/Pages/EsqueciSenha.js
+++ frontend/src/Pages/EsqueciSenha.js
@@ -163,7 +163,7 @@ const handleRedefinirSenha = async (e) => {
 
           <Col>
               <div className="bloco-aviso">
-                <svg className="iconWarning" xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-triangle-alert"><path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3"/><path d="M12 9v4"/><path d="M12 17h.01"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-triangle-alert iconWarning"><path d="m21.73 18-8-14a2 2 0 0 0-3.48 0l-8 14A2 2 0 0 0 4 21h16a2 2 0 0 0 1.73-3"/><path d="M12 9v4"/><path d="M12 17h.01"/></svg>
                 <h5><b>Aviso: </b>Se você não configurou uma pergunta de segurança previamente, não será possível recuperar sua conta por este método.</h5>
               </div>
           </Col>
