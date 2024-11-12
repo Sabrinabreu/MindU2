321c2335bc6718a5e36896c2817cbd6c4e89d1f1
JessicaAJesus <jessicaarruda06@gmail.com>
2024-11-12 08:25:01 -0300

    navbar atualizada

--- frontend/src/Components/Navegacao.jsx
+++ frontend/src/Components/Navegacao.jsx
@@ -14,6 +14,7 @@ const Navegacao = ({ isDarkMode, toggleTheme }) => {
   const { token } = useAuth();
   const decodedToken = token ? parseJwt(token) : null;
   const tipoUsuario = decodedToken?.tipo_usuario;
+  const loginMethod = decodedToken?.loginMethod;
 
   const isActive = (path) => location.pathname === path;
 
@@ -22,7 +23,6 @@ const Navegacao = ({ isDarkMode, toggleTheme }) => {
     localStorage.setItem('dark-mode', isDarkMode);
   }, [isDarkMode]);
 
-  // Função para alternar o modo escuro com Espaço
   const handleKeyDown = (event) => {
     if (event.key === ' ') {
       event.preventDefault();
@@ -31,77 +31,71 @@ const Navegacao = ({ isDarkMode, toggleTheme }) => {
   };
 
   const links = [
-    { nome: "Home", caminho: "/", auth: false, tiposPermitidos: [] }, // Acessível para todos
-    { nome: "Contato", caminho: "/contato", auth: false, tiposPermitidos: [] }, // Acessível para todos
-    { nome: "Planos", caminho: "/planos", auth: false, tiposPermitidos: ["empresa"] }, // Acessível para empresas logadas
-    { nome: "Cadastre-se como psicólogo", caminho: "/cadastroPsicologos", auth: false, tiposPermitidos: [""] }, // Acessível apenas para não logados
-    { nome: "Cadastro Empresa", caminho: "/cadastroEmpresa", auth: false, tiposPermitidos: [""] }, // Acessível apenas para não logados
-    { nome: "Login", caminho: "/login", auth: false, tiposPermitidos: [""] }, // Acessível apenas para não logados
-    { nome: "Acesso Funcionários", caminho: "/acessoFuncionarios", auth: true, tiposPermitidos: ["empresa"] }, // Apenas empresa logada
-    { nome: "Dashboard", caminho: "/dashboard", auth: true, tiposPermitidos: ["empresa"] }, // Apenas empresa logada
-    { nome: "Disponibilidade", caminho: "/disponibilidade", auth: true, tiposPermitidos: ["psicologo"] }, // Apenas psicólogo logado
-    { nome: "Perfil", caminho: "/perfil", auth: true, tiposPermitidos: ["psicologo", "funcionario"] }, // Psicólogos e funcionários logados
+    { nome: "Home", caminho: "/", auth: false, tiposPermitidos: [] },
+    { nome: "Contato", caminho: "/contato", auth: false, tiposPermitidos: [] },
+    { nome: "Planos", caminho: "/planos", auth: false, tiposPermitidos: ["empresa"] },
+    { nome: "Cadastre-se como psicólogo", caminho: "/cadastroPsicologos", auth: false, tiposPermitidos: [""] },
+    { nome: "Cadastro Empresa", caminho: "/cadastroEmpresa", auth: false, tiposPermitidos: [""] },
+    { nome: "Login", caminho: "/login", auth: false, tiposPermitidos: [""] },
+    { nome: "Acesso Funcionários", caminho: "/acessoFuncionarios", auth: true, tiposPermitidos: ["empresa"] },
+    { nome: "Dashboard", caminho: "/dashboard", auth: true, tiposPermitidos: ["empresa"] },
+    { nome: "Disponibilidade", caminho: "/disponibilidade", auth: true, tiposPermitidos: ["psicologo"] },
+    { nome: "Perfil", caminho: "/perfil", auth: true, tiposPermitidos: ["psicologo", "funcionario"] },
     { nome: "Agendar Consulta", caminho: "/agendarConsulta", auth: true, tiposPermitidos: ["psicologo", "funcionario"] },
-    { nome: "Quiz", caminho: "/quiz", auth: true, tiposPermitidos: ["funcionario"] }, // Psicólogos e funcionários logados
+    { nome: "Quiz", caminho: "/quiz", auth: true, tiposPermitidos: ["funcionario"] },
   ];
-  
-  const canAccessLink = (link, token, tipoUsuario) => {
-    // Links acessíveis para todos (auth: false e tiposPermitidos vazio)
+
+  const canAccessLink = (link) => {
     if (!link.auth && link.tiposPermitidos.length === 0) {
       return true;
     }
-  
-    // Links acessíveis apenas para não logados ou empresas (Planos)
     if (!link.auth && (!token || link.tiposPermitidos.includes(tipoUsuario))) {
       return true;
     }
-  
-    // Links acessíveis para usuários logados com tipos específicos
     if (token && link.auth && link.tiposPermitidos.includes(tipoUsuario)) {
       return true;
     }
-  
-    return false; // Não permitido
+    return false;
   };
-  
-    
 
   return (
     <Navbar expand="lg" className={`custom-navbar ${isDarkMode ? 'dark-mode' : ''}`}>
       <Container className='navContainer'>
-      <Acessibilidade toggleTheme={toggleTheme}/>
+        <Acessibilidade toggleTheme={toggleTheme}/>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <div className='navCentro mx-auto'>
-        <Navbar.Brand as={Link} to="/">
-          <img width={"30px"} src={Logo} alt="Logo" />
-        </Navbar.Brand>
-        <Nav>
-  {links.map((link, index) => {
-    // Oculta as páginas de login/cadastro se o usuário estiver logado
-    if (token && (link.caminho === "/login" || link.caminho.includes("cadastro"))) {
-      return null;
-    }
+            <Navbar.Brand as={Link} to="/">
+              <img width={"30px"} src={Logo} alt="Logo" />
+            </Navbar.Brand>
+            <Nav>
+              {links.map((link, index) => {
+                if (token && (link.caminho === "/login" || link.caminho.includes("cadastro"))) {
+                  return null;
+                }
 
-    // Verifica se o link pode ser acessado pelo usuário atual
-        if (canAccessLink(link, token, tipoUsuario)) {
-          return (
-            <Nav.Link
-              key={index}
-              as={Link}
-              to={link.caminho}
-              className={isActive(link.caminho) ? "active" : ""}
-              aria-current={isActive(link.caminho) ? "page" : undefined}
-            >
-              {link.nome}
-            </Nav.Link>
-          );
-        }
-    
-        return null; // Não exibe o link se o usuário não tiver permissão
-      })}
-    </Nav>
-</div>
+                const isDisabled = loginMethod === 'login_temporario' && 
+                  (link.caminho === '/quiz' || link.caminho === '/agendarConsulta');
+
+                if (canAccessLink(link)) {
+                  return (
+                    <Nav.Link
+                      key={index}
+                      as={Link}
+                      to={isDisabled ? "#" : link.caminho} // Redireciona para "#" se o link estiver desativado
+                      className={`${isActive(link.caminho) ? "active" : ""} ${isDisabled ? "disabled-link" : ""}`}
+                      aria-current={isActive(link.caminho) ? "page" : undefined}
+                      tabIndex={isDisabled ? -1 : 0} // Remove o foco para links desativados
+                    >
+                      {link.nome}
+                    </Nav.Link>
+                  );
+                }
+
+                return null;
+              })}
+            </Nav>
+          </div>
           <div className='button-toggle'>
             <label
               className="switch"
@@ -120,7 +114,6 @@ const Navegacao = ({ isDarkMode, toggleTheme }) => {
             </label>
           </div>
         </Navbar.Collapse>
-        
       </Container>
     </Navbar>
   );
